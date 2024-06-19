959bbc325b3f3c96db918380909d2cd1034d7ad2
Dejan <ddimitrijevicdev@gmail.com>
2024-06-20 05:40:37 +1000

    completed Homepage

--- assets/c-swiper.js
+++ assets/c-swiper.js
@@ -2,7 +2,8 @@
     let fadeEffectSpeed = document.querySelector('.fade_effect_speed').dataset.fadeEffectSpeed;
 
     let featuredColumnsNumber = document.querySelector('.featured-columns-number').dataset.columnsNumber;
-    console.log("featuredColumnsNumber", featuredColumnsNumber)
+    let featuredColumnsNumberMobile = document.querySelector('.featured-columns-number-mobile').dataset.columnsNumberMobile;
+    console.log("featuredColumnsNumberMobile", featuredColumnsNumberMobile)
 
 
   let featuredOption = {
@@ -11,12 +12,32 @@
     slidesPerView: featuredColumnsNumber,
     spaceBetween: 35,
     centeredSlides: false,
+    freeMode: true,
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      dragSize: 87,
+    },
+    mousewheel: true,
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    }
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: featuredColumnsNumberMobile,
+          spaceBetween: 16,
+       },
+       767: {
+        slidesPerView: featuredColumnsNumber - 1,
+         spaceBetween: 24,
+       },
+       1024: {
+        slidesPerView: featuredColumnsNumber,
+         spaceBetween: 35,
+       },
   }
+}
 
   let fadeBannerOption = {
         slidesPerView: 1,
@@ -30,11 +51,11 @@
             clickable: true,
         },
         loop: true,
-        autoplay: {
-            delay: sliderRangeDelay,
-        },
+        // autoplay: {
+        //     delay: sliderRangeDelay,
+        // },
         speed: fadeEffectSpeed
   }
 
-let looksSliderFeaturedOption = new Swiper('.swiper-featured-collection', featuredOption);
+let looksSliderFeaturedOption = new Swiper('.horizontal_scroll_swiper', featuredOption);
 let looksSliderFadeBannerOption = new Swiper('.swiper-fade-banner', fadeBannerOption);
