f5a285d494d3bdaff9e8d0baece69bc5fb46f95b
dejan <ddimtrijevicdev@gmail.com>
2024-07-09 01:05:40 +1000

    updated product-media-gallery add swiper-slider

---
+++ assets/c-product-media-gallery-swiper.js
@@ -0,0 +1,10 @@
+
+let productMediaGallery = {
+    // Optional parameters
+loop: true,
+slidesPerView: 5,
+spaceBetween: 10,
+}
+
+
+let productMediaGalleryOption = new Swiper('.product-media-gallery', productMediaGallery);
